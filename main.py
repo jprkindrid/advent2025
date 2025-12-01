b5c67b9648dd3aa56475a89c3691ed5f1518df7a
jprkindrid <jpriveraa7@gmail.com>
2025-12-01 11:45:23 -0600

    day 1

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+    current_point = 50
+    crosses_count = 0
+    zero_count = 0
+
+    with open("input1.txt") as f:
+        for rotation in f:
+            distance = int(rotation[1:])
+
+            if rotation.startswith("L"):
+                crosses_count -= current_point == 0
+                current_point -= distance
+                crosses_count -= (current_point // 100) - (1 if current_point % 100 == 0 else 0)
+    
+            else:
+                current_point += distance
+                crosses_count += current_point // 100
+
+            current_point = current_point % 100
+
+            if current_point == 0:
+                zero_count += 1
+
+    print(f"Final Cross Count: {crosses_count}")
+    print(f"Final Zero Count: {zero_count}")
+
+
+if __name__ == "__main__":
+    main()
