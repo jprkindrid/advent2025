930ee56e4437e9deab3758bde29d7444c7f0a201
jprkindrid <jpriveraa7@gmail.com>
2025-12-04 13:07:05 -0600

    drying my day 4 because why not

--- day4.py
+++ day4.py
@@ -1,8 +1,22 @@
 from typing import List, Set, Tuple
 
+def can_remove(roll, roll_positions) -> bool:
+    adjacent = 0
+    for dx in [-1, 0, 1]:
+        if adjacent > 3:
+            return False
+        for dy in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue
+            if (roll[0] + dx, roll[1] + dy) in roll_positions:
+                adjacent += 1
+                if adjacent > 3:
+                    return False
+                
+    return adjacent < 4
+
 def day4():
-    roll = "@"
-    empty = "."
+    roll_char = "@"
     total1 = 0
     total2 = 0
     total_rolls = 0
@@ -13,25 +27,14 @@ def day4():
 
             for x, ch in enumerate(line):
                 total_rolls += 1
-                if ch == roll:
+                if ch == roll_char:
                     roll_list.append((x, y))
 
     roll_positions: Set[Tuple[int, int]] = set(roll_list)
 
     #part 1
     for roll in roll_list:
-        adjacent = 0
-        for dx in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
-                if dx == 0 and dy == 0:
-                    continue
-                if (roll[0] + dx, roll[1] + dy) in roll_positions:
-                    adjacent += 1
-                    if adjacent > 3:
-                        break
-        
-
-        if adjacent < 4:
+        if can_remove(roll, roll_positions):
             total1 += 1
 
     # part 2:
@@ -39,25 +42,12 @@ def day4():
     while processing:
         processing = False
         for i, roll in enumerate(roll_list):
-            adjacent = 0
-            for dx in [-1, 0, 1]:
-                for dy in [-1, 0, 1]:
-                    if dx == 0 and dy == 0:
-                        continue
-                    if (roll[0] + dx, roll[1] + dy) in roll_positions:
-                        adjacent += 1
-                        if adjacent > 3:
-                            break
-            
-
-            if adjacent < 4:
+            if can_remove(roll, roll_positions):
                 processing = True
                 del roll_list [i]
                 roll_positions.remove(roll)
                 total2 += 1
                 continue
-                
-
 
     print(f"Total1: {total1}")    
     print(f"Total2: {total2}")    
