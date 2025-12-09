de7c5d605e92f74fd2a7dabc36d2b5296191aa96
jprkindrid <jpriveraa7@gmail.com>
2025-12-09 00:23:46 -0600

    day 9 part 1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from math import sqrt
-filename = "input8.txt"
-num_connections = 10 if "example" in filename else 1000
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+input_path = os.path.join(current_dir, "inputs/input9.txt")
 
 
 def main():
@@ -8,58 +8,27 @@ def main():
     total2 = 0
 
     points = []
-    with open(filename) as f:
+    with open(input_path) as f:
         for line in f:
-            x,y,z = map(int, line.strip().split(","))
-            points.append((x,y,z))
-                    
-    def get_distance(p1, p2):
-        return sqrt(sum((a - b) ** 2 for a, b in zip(p1, p2)))
-        # sqrt((x1-x2)^2+(y1-y2)^2+(z1-z2)^2) euclidian distance formula thanks claude
-    
-    pairs = []
+            x,y= map(int, line.strip().split(","))
+            points.append((x,y))
+
+    # part 1
+    max_area = 0
     for i in range(len(points)):
         for j in range(i + 1, len(points)):
-            distance = get_distance(points[i], points[j])
-            pairs.append((distance, i, j))
-            
-    pairs.sort()
-    
-    parent = list(range(len(points)))
-    def find_root(x):
-        if parent[x] != x:
-            parent[x] = find_root(parent[x])
-        return parent[x]
-    
-    def find_union(x, y):
-        px, py = find_root(x), find_root(y)
-        if px != py:
-            parent[px] = py
-            return True
-        return False
-    
-    connected = 0
-    last_i, last_j = None, None
-    for iteration, (_, i, j) in enumerate(pairs):
-        if find_union(i, j):
-            connected += 1
-            last_i, last_j = i, j
+            x1, y1 = points[i]
+            x2, y2 = points[j]
 
-            # part 1 
-            if iteration == num_connections - 1:
-                circuts = {}
-                for k in range(len(points)):
-                    root = find_root(k)
-                    if root not in circuts:
-                        circuts[root] = 0
-                    circuts[root] += 1
-                sizes_count = sorted(circuts.values(), reverse=True)
-                total1 = sizes_count[0] * sizes_count[1] * sizes_count[2]
+            width = abs(x1 - x2) + 1
+            height = abs(y1 - y2) + 1
+            area = width * height
+            max_area = max(max_area, area)
 
-            # part 2
-            if connected == len(points) - 1:
-                total2 = points[last_i][0] * points[last_j][0]
-                break
+    
+    total1 = max_area
+    # part 2
+    # for i in range(len(points))
 
     print(f"Total1: {total1}")    
     print(f"Total2: {total2}")
