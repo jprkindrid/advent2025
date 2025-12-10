88d3d9fe9b53bc4cbab063ff31bbbec71e640eef
jprkindrid <jpriveraa7@gmail.com>
2025-12-10 00:28:38 -0600

    day 10 part 1 and infinitely long part 2

--- main.py
+++ main.py
@@ -1,58 +1,129 @@
-from typing import List, Set, Tuple
+from typing import List, Tuple
 import os
+from collections import deque
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-input_path = os.path.join(current_dir, "inputs/input9.txt")
+input_path = os.path.join(current_dir, "inputs/input10.txt")
+
+def lights_to_int(diagram):
+    val = 0
+    for i, c in enumerate(diagram):
+        if c == "#":
+            val |= (1 << i)
+    return val
+
+def make_mask(button):
+    m = 0
+    for i in button:
+        m |= (1 << i)
+    return m
+
+def min_presses_diagram(light_diagram, buttons):
+    start = 0
+    target = lights_to_int(light_diagram)
+
+    button_masks = [make_mask(button) for button in buttons]
+
+    q = deque()
+    q.append((start, 0))
+    seen = {start}
+
+    while q:
+        state, steps = q.popleft()
+
+        if state == target:
+            return steps
+        
+        for mask in button_masks:
+            nxt = state ^ mask
+            if nxt not in seen:
+                seen.add(nxt)
+                q.append((nxt, steps + 1))
+    
+    return None
+
+def min_presses_joltage(target, buttons):
+    dimension = len(target)
+    start = tuple([0] * dimension)
+    adds = []
+    for b in buttons:
+        vec = [0] * dimension
+        for i in b:
+            vec[i] += 1
+        adds.append(tuple(vec))
+    
+    q = deque()
+    q.append((start, 0))
+    seen = {start}
+
+    while q:
+        state, steps = q.popleft()
+
+        if state == tuple(target):
+            return steps
+        
+        for add in adds:
+            n = tuple(state[i] + add[i] for i in range(dimension))
+            if any(n[i] > target[i] for i in range(dimension)):
+                continue  
+            
+            if n not in seen:
+                seen.add(n)
+                q.append((n, steps + 1))
+
+            if n not in seen:
+                seen.add(n)
+                q.append((n, steps + 1))
+
+    return None
+
+
+def get_sections(machine):
+    parts = machine.split(" ")
+    light_diagram = ""
+    buttons = []
+    joltages = []
+
+    for part in parts:
+        if part.startswith("["):
+            for c in part:
+                if c in ".#":
+                    light_diagram += c
+
+        elif part.startswith("("):
+            inner = part[1:-1].replace(" ", "")
+            nums = [int(x) for x in inner.split(",") if x]
+            buttons.append(tuple(nums))
+
+        elif part.startswith("{"):
+            inner = part[1:-1].replace(" ", "")
+            nums = [int(x) for x in inner.split(",") if x]
+            joltages.extend(nums)
+
+    return light_diagram, buttons, joltages
 
 def main():
     total1 = 0
     total2 = 0
 
-    vertices: List[Tuple[int, int]] = []
     with open(input_path) as f:
-        for line in f:
-            x, y = map(int, line.strip().split(","))
-            vertices.append((x, y))
+        machines = [line.strip() for line in f]
 
-    # part 1
-    for i, (x1, y1) in enumerate(vertices):
-        for x2, y2 in vertices[i + 1:]:
-            width = abs(x1 - x2) + 1
-            height = abs(y1 - y2) + 1
-            total1 = max(total1, width * height)
+    for i, m in enumerate(machines):
+        light_diagram, buttons, joltages = get_sections(m)
 
-    # part 2
-    edge_points: Set[Tuple[int, int]] = set()
-    for idx, (x1, y1) in enumerate(vertices):
-        x2, y2 = vertices[(idx + 1) % len(vertices)]
-        if y1 == y2:
-            for x in range(min(x1, x2), max(x1, x2) + 1):
-                edge_points.add((x, y1))
-        else:
-            for y in range(min(y1, y2), max(y1, y2) + 1):
-                edge_points.add((x1, y))
+        toggle_presses = min_presses_diagram(light_diagram, buttons)
 
-    # I realized after reading around that checking if the complex object (bounding box) perimiter intersects the simple object (rectangle) is probably much faster
-    for i, (x1, y1) in enumerate(vertices):
-        for x2, y2 in vertices[i + 1:]:
-            width = abs(x1 - x2) + 1
-            height = abs(y1 - y2) + 1
-            area = width * height
+        if toggle_presses is not None:
+            total1 += toggle_presses
 
-            if area <= total2:
-                continue
+        joltage_presses = min_presses_joltage(joltages, buttons)
+        
+        if joltage_presses is not None:
+            total2 += joltage_presses
 
-            min_x, max_x = min(x1, x2), max(x1, x2)
-            min_y, max_y = min(y1, y2), max(y1, y2)
 
-            valid = True
-            for ex, ey in edge_points:
-                if min_x < ex < max_x and min_y < ey < max_y:
-                    valid = False
-                    break
-
-            if valid:
-                total2 = area
+        # print(f"Machine {i} presses: {presses}")
 
     print(f"Total1: {total1}")
     print(f"Total2: {total2}")
